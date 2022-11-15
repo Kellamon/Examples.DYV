38fc572b402aabc95d269bb4735f7209b0039e79
Kellamon <badzed@list.ru>
2022-11-15 10:05:27 +0300

    Ищем значение в массиве

---
+++ Example008/Program.cs
@@ -0,0 +1,18 @@
+﻿// Одномерный массив array из n элементовб требуется найти элемент массив find
+int[] array = {1,23,34,46,57,68,79,81};
+
+int n = array.Length;
+int find = 68;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break; // дает разрыв в выполнении команды
+    }
+    // index = index + 1;
+    index++;
+}
